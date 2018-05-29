be0811d2effb702accc4d3a195f96f45ffe6b7be
Connor Yates <cl.yates.42@gmail.com>
2018-05-28 23:09:56 -0700

    Fix the naming convention of rosaria within this package so the topics communicate properly.
    Update the velocity calculation to:
    1. Use the angle of the tag to the camera, not just the relative y pos
    2. Bin the angles to corresponding velocities
    3. Add a dead zone to squash the oscilliations seen previously

--- src/track_ar_tag_angle.py
+++ src/track_ar_tag_angle.py
@@ -12,8 +12,7 @@ class AimingVelocity():
     aims the robot
     '''
     def __init__(self):
-        self.pub = rospy.Publisher("/RosAria/cmd_vel", Twist, queue_size=10)
-        self.vel_mag = 0.2
+        self.pub = rospy.Publisher("/rosaria/cmd_vel", Twist, queue_size=10)
     def send_vel(self, msg):
         '''
         Calculates which velocity should be sent to the base of the robot
@@ -22,13 +21,22 @@ class AimingVelocity():
         if len(msg.markers) == 0:
             print "empty"
             return
+        x_relative = msg.markers[0].pose.pose.position.x
         y_relative = msg.markers[0].pose.pose.position.y
+        angle = math.atan(abs(y_relative)/abs(x_relative))
+        print "ANGLE: ", angle
+        if angle > 0.2:
+            vel_mag = 0.2
+        elif angle > 0.05:
+            vel_mag = 0.1
+        else:
+            vel_mag = 0
         twist_msg = Twist()
         if y_relative > 0:
-            twist_msg.angular.z = self.vel_mag
+            twist_msg.angular.z = vel_mag
             print "To the left"
         elif y_relative < 0:
-            twist_msg.angular.z = -self.vel_mag
+            twist_msg.angular.z = -vel_mag
             print "to the right"
         else:
             print "Dead center"
